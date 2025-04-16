2a118ef791204972d7e2434bb6b4bf9e9a9dd082
Sayan <mahantosayan@gmail.com>
2025-04-16 22:01:41 +0530

    Structure of Deal Mitra

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Dashboard from "./dashboard/Dashboard";
+import Login from "./login/Login";
+import Signup from "./signup/Signup";
+import SearchPage from "./searchpage/SearchPage";
+import FilterPage from "./filterpage/FilterPage";
+
+export { Dashboard, Login, Signup, SearchPage, FilterPage };
